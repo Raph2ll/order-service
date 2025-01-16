9072d025161fd1590d72598b922fa3f978c969a7
Raph2ll <raphael.ph2ll@gmail.com>
2025-01-16 18:50:14 -0300

    fix: id

--- models/customer_model.py
+++ models/customer_model.py
@@ -9,6 +9,6 @@ class Customer(BaseModel):
     """
     Represents a customer with information such as name, email and status.
     """
-    id: str = Field(..., description="Unique customer identifier in ULID format.")
+    customer_id: str = Field(..., description="Unique customer identifier in ULID format.")
     name: str = Field(..., description="Customer name")
     email: EmailStr = Field(..., description="Valid customer email adress")
